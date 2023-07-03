a4971acd75be90e6ea6ba0b94e29896b80a06527
Parth Aggarwal <aparth33@yahoo.co.in>
2023-07-04 01:02:45 +0530

    creation of user

--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -2,7 +2,8 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/',(req, res)=>{
-    res.json([]);
+    console.log(req.body)
+    res.json(req.body);
 })
 
 module.exports = router;
