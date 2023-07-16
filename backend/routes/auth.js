561e751a7031f32806dad12d1faa99a4fd3d418e
Parth Aggarwal <aparth33@yahoo.co.in>
2023-07-17 03:31:38 +0530

    added changes for authentication

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,24 +1,57 @@
-const express = require('express');
-const User = require('../models/User');
+const express = require("express");
+const User = require("../models/User");
 const router = express.Router();
-const { body, validationResult } = require('express-validator');
+const { body, validationResult } = require("express-validator");
+const bcrypt = require("bcryptjs")
+const JWT_SECRET = "parthisgoodboy"
+const jwt=require("jsonwebtoken")
+router.post(
+  "/createUser",
+  [
+    body("name", "Enter a valid name").isLength({ min: 3 }),
+    body("email", "Enter a valid email").isEmail(),
+    body("password", "Enter a valid password").isLength({ min: 5 }),
+  ],
+  async (req, res) => {
+    try {
+      //if there are errors, return Bad request and the errors
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+      }
+      //check whether user with this email exists already
+      let user = await User.findOne({ email: req.body.email });
+      if (user) {
+        return res
+          .status(400)
+          .json({ error: "Sorry a user with this email already exists" });
+      }
+      const salt=await bcrypt.genSalt(10);
+      const secPass=await bcrypt.hash(req.body.password,salt);
 
-router.post('/',[
-    body('name',"Enter a valid name").isLength({min:3}),
-    body('email',"Enter a valid email").isEmail(),
-    body('password',"Enter a valid password").isLength({min:5}),
-],(req, res)=>{
-    const errors=validationResult(req);
-    if(!errors.isEmpty()){
-        return res.status(400).json({errors:errors.array()});
+      user = await User.create({
+        name: req.body.name,
+        email: req.body.email,
+        password: secPass,
+      });
+
+      const data={
+        user:{
+          id:user.id
+        }
+      }
+
+      const authToken=jwt.sign(data,JWT_SECRET)
+      // res.json(user);
+      res.json({authToken})
+      // .then(user=>res.json(user))
+      // .catch(err=>{console.log(err)
+      // res.json({error:"Please enter a unique value for email", message:err.message})})
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send("Some error occured");
     }
-    User.create({
-        name:req.body.name,
-        email:req.body.email,
-        password:req.body.password,
-    }).then(user=>res.json(user))
-    .catch(err=>{console.log(err)
-    res.json({error:"Please enter a unique value for email", message:err.message})})
-})
+  }
+);
 
 module.exports = router;
